f726b2b9e06a4b32c58fc018359bcb34c2b13e79
noyan <noyan558@gmail.com>
2021-12-01 03:06:17 +0300

    Some Variable Change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 var port = 3000;
 var app = express();
 var wikiUrls1 = "";
-// Warte67 Branch'ine aittir
+var Warte = "Warte12345";
 rp(url)
   .then(function(html){
     //success!
